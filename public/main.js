93c949a35371a84e05b91678637e5d215a739d93
WyattReed01 <wickdshot@gmail.com>
2022-09-05 09:24:58 -0500

    updated public folder

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
-function newItem(event){
-    event.preventDefault();
-    let input = document.getElementById("input")
-    console.log(input.value)
-}
+const list = document.querySelector('#list')
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
 
 
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const text = input.value
+    const li = document.createElement('li')
+    li.className ='list-item'
+    li.textContent = text;
+    list.appendChild(li)
+    input.value = "";
+    console.log(text)
+})
