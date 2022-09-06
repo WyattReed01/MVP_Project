0d8723775c7ca5077a3d5962cae80362048a3692
WyattReed01 <wickdshot@gmail.com>
2022-09-06 03:54:09 -0500

    testing fetch api calls

--- public/main.js
+++ public/main.js
@@ -2,8 +2,10 @@ const list = document.querySelector('#list')
 const form = document.querySelector('#form')
 const input = document.querySelector('#input')
 const btn = document.querySelector('#btn')
+const apiURL = "https://sleepy-eyrie-67463.herokuapp.com/tasks"
 
 
+//somehow tranform these into crud actions that call the database
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const text = input.value
@@ -12,5 +14,39 @@ form.addEventListener('submit', (e)=>{
     li.textContent = text;
     list.appendChild(li)
     input.value = "";
-    console.log(text)
 })
+
+//post fetch
+form.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+    // const jsonDa
+    const data = input.value
+    const addData = await fetch(apiURL + '/tasks', {
+        method: 'POST'
+    }).then(function (response) {
+        // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+})
+
+
+
+
+
+// get all fetch
+
+// get 1 fetch
+
+//put fetch
+
+//delete all fetch
