8f570fbf46d25207b3eef1728f71180ddddec086
WyattReed01 <wickdshot@gmail.com>
2022-09-06 04:15:35 -0500

    updated fetch request

--- public/main.js
+++ public/main.js
@@ -6,38 +6,37 @@ const apiURL = "https://sleepy-eyrie-67463.herokuapp.com"
 
 
 //somehow tranform these into crud actions that call the database
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const text = input.value
     const li = document.createElement('li')
-    li.className ='list-item'
+    li.className = 'list-item'
     li.textContent = text;
     list.appendChild(li)
     input.value = "";
 })
 
 //post fetch
-form.addEventListener('submit', async(e) =>{
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    // const jsonDa
-    const data = input.value
-    const addData = await fetch(apiURL + '/tasks', {
-        method: 'POST'
-    }).then(function (response) {
-        // The API call was successful!
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    }).then(function (data) {
-        // This is the JSON from our response
-        console.log(data);
-    }).catch(function (err) {
-        // There was an error
-        console.warn('Something went wrong.', err);
-    });
-})
+    const body = { description };
+    const response = await fetch(apiURL + '/tasks', {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+        .then((response) => response.json())
+        .then((body) => {
+            // This is the JSON from our response
+            console.log(body);
+        })
+        .catch((err) => {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        });
+
+});
+
+
 
 
 
